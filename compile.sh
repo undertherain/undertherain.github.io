57fe172faf6dcad26bccd0d588e153d663d0fda1
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-07-07 16:48:06 +0900

    update compile

--- compile.sh
+++ compile.sh
@@ -3,6 +3,11 @@
 #dev
 #NODE_ENV=development ./themes/blowfish/node_modules/tailwindcss/lib/cli.js -c ./themes/blowfish/tailwind.config.js -i ./themes/blowfish/assets/css/main.css -o ./assets/css/compiled/main.css --jit -w",
 #build
-NODE_ENV=production ./themes/blowfish/node_modules/tailwindcss/lib/cli.js -c ./themes/blowfish/tailwind.config.js -i ./themes/blowfish/assets/css/main.css -o ./assets/css/compiled/main.css --jit
-
+NODE_ENV=production \
+    npx ./themes/blowfish/node_modules/@tailwindcss/cli \
+    -i ./assets/css/main.css \
+    -c ./themes/blowfish/tailwind.config.js \
+    -o ./assets/css/compiled/main.css --jit
+#-i "./themes/blowfish/assets/css/main.css  ./static/css/quiz.css" \
+#-i ./static/css/quiz.css \
 # TODO: modify theme's config to add additional colors
